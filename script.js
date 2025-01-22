9cc5f91e9b190b3df4b691a9579ab195b4c47946
sahiba0915 <sahibakumari0915@outlook.com>
2025-01-22 17:21:37 +0530

    feat: added functionality for the game

---
+++ script.js
@@ -0,0 +1,44 @@
+const playBoard = document.querySelector(".play-board");
+
+let foodX = 13 , foodY = 10;
+let snakeX = 13 , snakeY = 10;
+let velocityX = 0, velocityY = 0
+
+const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const changeDirection = (e) => {
+    if(e.key === "ArrowUp"){
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.key === "ArrowDown"){
+        velocityX = 0;
+        velocityY = 1;
+    }else if(e.key === "ArrowLeft"){
+        velocityX = -1;
+        velocityY = 0;
+    }else if(e.key === "ArrowRight"){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+const initGame = () => {
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY}/ ${foodX}"></div>`;
+
+    if(snakeX === foodX && snakeY === foodY){
+        changeFoodPosition();
+    }
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeY}/ ${snakeX}"></div>`;
+    playBoard.innerHTML= htmlMarkup;
+}
+
+changeFoodPosition();
+setInterval(initGame, 125);
+document.addEventListener("keydown", changeDirection)
